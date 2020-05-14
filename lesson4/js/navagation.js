dc4c055dc11fe35fec9d85847cd0bd0e2941a024
joshuakfullmer <joshuakfullmer@byui.edu>
2020-05-14 14:28:27 -0600

    how about now?

--- lesson4/js/navagation.js
+++ lesson4/js/navagation.js
@@ -1,6 +1,9 @@
 document.getElementById('update').innerHTML = document.lastModified;
 document.getElementById('year').innerHTML = new Date().getFullYear();
-const hambutton = document.querySelector('.ham');
-const mainnav = document.querySelector('.navbar')
+function toggleMenu() {
+    document.getElementsByClassName('navbar')[0].classList.toggle('respons')
+    // const hambutton = document.querySelector('.ham');
+    // const mainnav = document.querySelector('.navbar')
 
-hambutton.addEventListener('click', () => {mainnav.classList.toggle('respons')}, false);
+    // hambutton.addEventListener('click', () => {mainnav.classList.toggle('respons')}, false);
+}
