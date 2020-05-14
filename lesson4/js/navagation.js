d22630938b186228f13b19d818c57794afcda83b
joshuakfullmer <joshuakfullmer@byui.edu>
2020-05-14 13:05:36 -0600

    js changes

---
+++ lesson4/js/navagation.js
@@ -0,0 +1,5 @@
+document.getElementById('update').innerHTML = document.lastModified;
+document.getElementById('year').innerHTML = new Date().getFullYear();
+const hambutton = document.querySelector('.ham');
+document.querySelector('navbar');
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
