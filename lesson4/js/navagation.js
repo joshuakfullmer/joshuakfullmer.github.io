e1505efffa160832a9e2ff5d0db86ff94519558c
joshuakfullmer <joshuakfullmer@byui.edu>
2020-05-14 13:52:51 -0600

    js fix

--- lesson4/js/navagation.js
+++ lesson4/js/navagation.js
@@ -1,5 +1,6 @@
 document.getElementById('update').innerHTML = document.lastModified;
 document.getElementById('year').innerHTML = new Date().getFullYear();
 const hambutton = document.querySelector('.ham');
-document.querySelector('navbar');
+const mainnav = document.querySelector('navbar')
+
 hambutton.addEventListener('click', () => {mainnav.classList.toggle('respons')}, false);
