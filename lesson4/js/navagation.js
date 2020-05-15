52238ebbc59205c3e62db579e2947fc7150addab
joshuakfullmer <joshuakfullmer@byui.edu>
2020-05-15 10:12:00 -0600

    responsive

--- lesson4/js/navagation.js
+++ lesson4/js/navagation.js
@@ -1,9 +1,9 @@
 document.getElementById('update').innerHTML = document.lastModified;
 document.getElementById('year').innerHTML = new Date().getFullYear();
 function toggleMenu() {
-    document.getElementsByClassName('navbar')[0].classList.toggle('respons')
+    document.getElementsByClassName('navbar')[0].classList.toggle('responsive')
     // const hambutton = document.querySelector('.ham');
     // const mainnav = document.querySelector('.navbar')
 
-    // hambutton.addEventListener('click', () => {mainnav.classList.toggle('respons')}, false);
+    // hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
 }
