b39a6457063fbf1607318a0a98e0d5d8d2084e61
joshuakfullmer <joshuakfullmer@byui.edu>
2020-05-20 12:07:36 -0600

    what day is it

--- lesson5/js/navagation.js
+++ lesson5/js/navagation.js
@@ -1,12 +1,12 @@
 
 // navagation
 function toggleMenu() {
-    document.getElementsByClassName('navbar')[0].classList.toggle('responsive')
+    document.getElementsByClassName('navbar')[0].classList.toggle('responsive');
 }
 // date
 document.getElementById("now").innerHTML = getDate();
 function getDate() {
-    let day = new Date()
+    let day = new Date();
     let month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     let dayOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     let now = dayOfWeek[day.getDay()] + ", " + day.getDate() + " " + month[day.getMonth()] + " " + day.getFullYear();
@@ -15,3 +15,11 @@ function getDate() {
 // last updated date
 document.getElementById('update').innerHTML = document.lastModified;
 document.getElementById('year').innerHTML = new Date().getFullYear();
+// banner
+function banner() {
+    let day = new Date();
+    if (day.getDay != 5) {
+        document.getElementsByClassName('banner')[0].classList.toggle('responsive');
+    }
+    document.getElementById('dayweek').innerHTML = day.getDay;
+}
