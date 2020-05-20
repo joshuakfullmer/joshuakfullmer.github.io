2d73bde5cb2f09dda0fdb116d3c2f42691f08bc5
joshuakfullmer <joshuakfullmer@byui.edu>
2020-05-20 11:05:35 -0600

    added lesson 5

---
+++ lesson5/js/navagation.js
@@ -0,0 +1,17 @@
+
+// navagation
+function toggleMenu() {
+    document.getElementsByClassName('navbar')[0].classList.toggle('responsive')
+}
+// date
+document.getElementById("now").innerHTML = getDate();
+function getDate() {
+    let day = new Date()
+    let month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let dayOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let now = dayOfWeek[day.getDay()] + ", " + day.getDate() + " " + month[day.getMonth()] + " " + day.getFullYear();
+    return now;
+}
+// last updated date
+document.getElementById('update').innerHTML = document.lastModified;
+document.getElementById('year').innerHTML = new Date().getFullYear();
