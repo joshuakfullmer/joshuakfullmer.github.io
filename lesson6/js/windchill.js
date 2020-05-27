522cbbba2183c22bce905dbc34bd9ecf172dc20f
joshuakfullmer <joshuakfullmer@byui.edu>
2020-05-27 15:56:47 -0600

    asdf

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,12 +1,15 @@
 
+function chill () {
     let windSpeed = 70;
     let air = 3;
+    let windChill;
     if (air >= 50 || air > 3) {
-        let windChill = 35.74 + .0215 * air - 35.75 * Math.pow(windSpeed, .16) + .4275 * air * Math.pow(windSpeed, .16);
+        windChill = 35.74 + .0215 * air - 35.75 * Math.pow(windSpeed, .16) + .4275 * air * Math.pow(windSpeed, .16);
     }
     else {
-        let windChill = "N/A"
+        windChill = "N/A";
     }
     document.getElementsByClassName('windspeedspan').innerHTML = windSpeed;
     document.getElementsByClassName('airspan').innerHTML = air;
     document.getElementsByClassName('windchillspan').innerHTML = windChill;
+}
