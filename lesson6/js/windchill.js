a38eff33e6ead0ae2a0287957a5ac88acc44cf48
joshuakfullmer <joshuakfullmer@byui.edu>
2020-05-29 14:52:11 -0600

    font fix home page

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -4,7 +4,7 @@ let air = 40;
 let windChill;
 if (air <= 50 && air > 3) {
     windChill = 35.74 + .0215 * air - 35.75 * Math.pow(windSpeed, .16) + .4275 * air * Math.pow(windSpeed, .16);
-    let digits = 1;
+    let digits = 0;
     let multiplier = Math.pow(10, digits);
     windChill = Math.round(windChill * multiplier) / multiplier;
     windChill += "°F";
