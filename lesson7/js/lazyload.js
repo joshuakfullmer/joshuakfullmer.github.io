c7bfb426dc6c677610947bd286b3fe2b87dbe9b0
joshuakfullmer <joshuakfullmer@byui.edu>
2020-06-03 15:42:12 -0600

    fixed image folder

--- lesson7/js/lazyload.js
+++ lesson7/js/lazyload.js
@@ -1,17 +1,15 @@
 
 // Text Book Method
-const imagesToLoad = document.querySelectorAll('img[data-src]');
-
+let imagesToLoad = document.querySelectorAll('img[data-src]');
+const imgOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px 50px 0px"
+};
 const loadImages = (image) => {
     image.setAttribute('src', image.getAttribute('data-src'));
     image.onload = () => {
         image.removeAttribute('data-src');
     };
-};
-
-const imgOptions = {
-    threshold: 0,
-    rootMargin: "0px 0px 50px 0px"
 };
 
 if('IntersectionObserver' in window) {
