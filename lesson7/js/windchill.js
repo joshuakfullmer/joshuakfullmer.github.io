5aaf53c712492b0789a6946adefb1e6e97295267
joshuakfullmer <joshuakfullmer@byui.edu>
2020-06-05 12:23:09 -0600

    changes to hovering and large view

--- lesson7/js/windchill.js
+++ lesson7/js/windchill.js
@@ -2,7 +2,7 @@
 let windSpeed = 11;
 let air = 40;
 let windChill;
-if (air <= 50 && air > 3) {
+if (air < 50 && air > 3) {
     windChill = 35.74 + .6215 * air - 35.75 * Math.pow(windSpeed, .16) + .4275 * air * Math.pow(windSpeed, .16);
     windChill = Math.round(windChill);
     windChill += "°F";
