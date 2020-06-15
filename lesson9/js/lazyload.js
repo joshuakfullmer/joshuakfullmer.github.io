04d455e7430f364f23bfa9812cfde8a0f835453f
joshuakfullmer <joshuakfullmer@byui.edu>
2020-06-15 15:41:57 -0600

    week 9

---
+++ lesson9/js/lazyload.js
@@ -0,0 +1,60 @@
+
+// Text Book Method
+let imagesToLoad = document.querySelectorAll('img[data-src]');
+const imgOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px 50px 0px"
+};
+const loadImages = (image) => {
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {
+        image.removeAttribute('data-src');
+    };
+};
+
+if('IntersectionObserver' in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+        items.forEach((item) => {
+            if(item.isIntersecting) {
+                loadImages(item.target);
+                observer.unobserve(item.target);
+            }
+        });
+    }, imgOptions);
+    imagesToLoad.forEach((img) => {
+        observer.observe(img);
+    });
+} 
+else {
+    imagesToLoad.forEach((img) => {
+        loadImages(img);
+    });
+}
+
+
+// Alternative Method
+/* const images = document.querySelectorAll('img[data-src]');
+
+const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            preloadImage(entry.target);
+            imgObserver.unobserve(entry.target);
+        }
+    })
+}, imgOptions);
+
+function preloadImage(img) {
+    const src = img.getAttribute('data-src');
+    if(!src) {
+        return;
+    }
+    img.src =src;
+}
+
+images.forEach(image => {
+    imgObserver.observe(image);
+});
+*/
