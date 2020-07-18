8428b3aed7760b013b3ff291e04887bb308328bd
joshuakfullmer <joshuakfullmer@byui.edu>
2020-07-18 11:59:05 -0600

    rental pages

--- final/js/rentaldatawrite.js
+++ final/js/rentaldatawrite.js
@@ -1,6 +1,5 @@
-const rentaldata = "js/rentaldata";
-
-fetch(rentaldata)
+const rentalurl = "https://joshuakfullmer.github.io/final/data/rentaldata.json";
+fetch(rentalurl)
     .then(function (response) {
         return response.json();
     })
@@ -15,10 +14,6 @@ fetch(rentaldata)
             // }
             let card = document.createElement('section');
             let h2 = document.createElement('h2');
-            let motto = document.createElement('p');
-            let yearFounded = document.createElement('p');
-            let currentPopulation = document.createElement('p');
-            let averageRainfall = document.createElement('p');
             let image = document.createElement('img');
             let divs = document.createElement('div');
             let imgdiv = document.createElement('div');
@@ -27,26 +22,35 @@ fetch(rentaldata)
 
             divs.appendChild(h2);
 
-            yearFounded.textContent = 'Year Founded: ' + rentals[i].yearFounded;
-
-            divs.appendChild(yearFounded);
-
-            currentPopulation.textContent = 'Population: ' + rentals[i].currentPopulation;
-
-            divs.appendChild(currentPopulation);
-
-            averageRainfall.textContent = 'Annual Rain Fall: ' + rentals[i].averageRainfall;
-
-            divs.appendChild(averageRainfall);
             card.appendChild(divs);
 
             image.setAttribute('src', 'images/' + rentals[i].photo);
-            image.setAttribute('alt', rentals[i].name + ' scenery');
+            image.setAttribute('alt', rentals[i].name);
             imgdiv.appendChild(image);
             
             card.appendChild(imgdiv);
             document.querySelector('div.cards').appendChild(card);
+            
+            document.getElementById(`ren${i + 1}`).textContent = rentals[i].name;
+            document.getElementById(`rhd${i + 1}`).textContent = rentals[i].rhd;
+            document.getElementById(`rfd${i + 1}`).textContent = rentals[i].rfd;
+            document.getElementById(`whd${i + 1}`).textContent = rentals[i].whd;
+            document.getElementById(`wfd${i + 1}`).textContent = rentals[i].wfd;
+            
 
             // document.querySelector('div.cards').appendChild(divs);
         }
+        // const fivedayforecast = jsObject.list.filter(x => x.dt_txt.includes('18:00:00'));
+        // const weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        // let i = 0;
+        // fivedayforecast.forEach(forecast => {
+        //     let day = new Date(forecast.dt_txt);
+        //     document.getElementById(`ren${i + 1}`).textContent = forecast.main.temp;
+        //     document.getElementById(`day${i + 1}`).textContent = weekday[day.getDay()];
+        //     document.getElementById(`icon${i + 1}`).src = 'https://openweathermap.org/img/w/' + forecast.weather[0].icon + '.png';
+        //     document.getElementById(`icon${i + 1}`).alt = forecast.weather[0].main + ' icon';
+        //     i++;
+        // });
+    });
+
     });
