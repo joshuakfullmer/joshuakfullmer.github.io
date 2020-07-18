9396aa52c5cf70b6e733b62704cf15dc34c82231
joshuakfullmer <joshuakfullmer@byui.edu>
2020-07-18 17:10:12 -0600

    first draft of rental page

--- final/js/rentaldatawrite.js
+++ final/js/rentaldatawrite.js
@@ -14,41 +14,37 @@ fetch(rentalurl)
             // }
             let card = document.createElement('section');
             let h2 = document.createElement('h2');
+            let p = document.createElement('p');
             let image = document.createElement('img');
             let divs = document.createElement('div');
             let imgdiv = document.createElement('div');
+            let single = false;
+            if (i == 0) {
+                single = true;
+            }
 
             h2.textContent = rentals[i].name;
-
-            divs.appendChild(h2);
-
-            card.appendChild(divs);
+            if (single) {
+                p.textContent = 'Seats ' + rentals[i].persons + ' person.';
+            } 
+            else {
+                p.textContent = 'Seats up to ' + rentals[i].persons + ' people.';
+            }
+            
+            card.appendChild(h2);
+            card.appendChild(p);
 
             image.setAttribute('src', 'images/' + rentals[i].photo);
             image.setAttribute('alt', rentals[i].name);
-            imgdiv.appendChild(image);
-            
-            card.appendChild(imgdiv);
+            image.setAttribute('class', 'cardpic');
+            card.appendChild(image);
+
             document.querySelector('div.cards').appendChild(card);
 
             document.getElementById(`ren${i + 1}`).textContent = rentals[i].name;
-            document.getElementById(`rhd${i + 1}`).textContent = rentals[i].rhd;
-            document.getElementById(`rfd${i + 1}`).textContent = rentals[i].rfd;
-            document.getElementById(`whd${i + 1}`).textContent = rentals[i].whd;
-            document.getElementById(`wfd${i + 1}`).textContent = rentals[i].wfd;
-            
-
-            // document.querySelector('div.cards').appendChild(divs);
-        }
-        // const fivedayforecast = jsObject.list.filter(x => x.dt_txt.includes('18:00:00'));
-        // const weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-        // let i = 0;
-        // fivedayforecast.forEach(forecast => {
-        //     let day = new Date(forecast.dt_txt);
-        //     document.getElementById(`ren${i + 1}`).textContent = forecast.main.temp;
-        //     document.getElementById(`day${i + 1}`).textContent = weekday[day.getDay()];
-        //     document.getElementById(`icon${i + 1}`).src = 'https://openweathermap.org/img/w/' + forecast.weather[0].icon + '.png';
-        //     document.getElementById(`icon${i + 1}`).alt = forecast.weather[0].main + ' icon';
-        //     i++;
-        // });
+            document.getElementById(`rhd${i + 1}`).textContent = "$" + rentals[i].rhd;
+            document.getElementById(`rfd${i + 1}`).textContent = "$" + rentals[i].rfd;
+            document.getElementById(`whd${i + 1}`).textContent = "$" + rentals[i].whd;
+            document.getElementById(`wfd${i + 1}`).textContent = "$" + rentals[i].wfd;
+            }
     });
